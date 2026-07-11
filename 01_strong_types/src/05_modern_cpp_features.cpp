d0a697ded8971674e6d7fbad3a9f05f5047ad283
sergey <adsv-soft@yandex.ru>
2026-07-11 15:44:59 +0300

    Refactor: remove duplicated code (discount logic and parameter object) from 05_modern_cpp_features.cpp since they are in their own files

--- 01_strong_types/src/05_modern_cpp_features.cpp
+++ 01_strong_types/src/05_modern_cpp_features.cpp
@@ -1,37 +1,12 @@
 #include <iostream>
-#include <cassert>
 
-namespace discount_features {
-
-    struct Price   { double value; };
-    struct Percent { double value; };
-    struct Rubles  { double value; };
-
-    // [2. Смысловая перегрузка интерфейсов]
-    Price discount(Price p, Percent pct) {
-        return Price{p.value * (1.0 - pct.value / 100.0)};
-    }
-
-    Price discount(Price p, Rubles off) {
-        return Price{p.value - off.value};
-    }
-
-    // [3. Гибкий порядок параметров]
-    Price discount(Rubles off, Price p) {
-        return discount(p, off);
-    }
-
-    void run() {
-        Price p{1000.0};
-        auto p1 = discount(p, Percent{10.0});
-        auto p2 = discount(Rubles{150.0}, p);
-
-        std::cout << "[04] Semantic overloading and flexible order works.\n";
-        assert(p1.value == 900.0);
-        assert(p2.value == 850.0);
-    }
-
-} // namespace discount_features
+/*
+ * Цикл статей: "C++: Техники за пределами прикладного минимума"
+ * Часть 1: Сильные типы
+ * 
+ * Пример 05: Современные возможности (C++11..C++23)
+ * Пользовательские литералы (UDL)
+ */
 
 namespace modern_cpp {
 
@@ -58,29 +33,7 @@ namespace modern_cpp {
 
 } // namespace modern_cpp
 
-namespace parameter_object {
-
-    // [Явная инициализация агрегатов]
-    struct SpeedParams {
-        double distance;
-        double time;
-    };
-
-    double speed(SpeedParams p) {
-        // Ограничение: внутри всё еще сырые double (нет защиты логики)
-        return p.distance / p.time;
-    }
-
-    void run() {
-        // Такой синтаксис помогает не ошибиться при вызове функции
-        auto v = speed({.distance = 100.0, .time = 5.0});
-        std::cout << "[06] Parameter Object works: " << v << "\n";
-    }
-
-} // namespace parameter_object
-
 int main() {
-    discount_features::run();
     modern_cpp::run();
-    parameter_object::run();
+    return 0;
 }
